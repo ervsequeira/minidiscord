277e110898ed1eaec0391f18f62c556868f64bf0
Erving Sequeira <esequeira.dev@gmail.com>
2026-09-08 17:15:42 -0600

    refactor: agregar requires java.desktop para usar XMLEncoder

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module cr.ac.una.est.minidiscord {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
     opens cr.ac.una.est.minidiscord.presentation.controller to javafx.fxml;
 
